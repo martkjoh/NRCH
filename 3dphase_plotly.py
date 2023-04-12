2ebbc5221c32991d1f910ebc536f3fbf1c528d29
Martin Johnsrud <martin.johnsrud@ds.mpg.de>
2023-04-12 17:57:19 +0200

    figures

--- 3dphase_plotly.py
+++ 3dphase_plotly.py
@@ -12,9 +12,7 @@ x0 = np.linspace(-k, k, N)
 r0 = np.linspace(-th, 0.001, N)
 x, r = np.meshgrid(x0, r0)
 a = np.sqrt(x**2**2 - (r + 2*x**2)**2 + 0j).real
-# a[0:2]=None
 a[0:2]=0
-a
 
 edge = np.zeros_like(a, dtype=bool)
 for i in ((0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1), (0, 2), (0, -2), (2, 0), (-2, 0)):
@@ -61,23 +59,24 @@ fig.add_surface(
     }
 )
 
-N = 1000
-x = np.linspace(-k/sqrt(2), k/sqrt(2), N)
-y = np.linspace(0, -th, N)
-x, y = np.meshgrid(x, y)
-z = x**2
 
-fig.add_surface(
-    x=x, y=y, z=z,
-    showscale=False, 
-    surfacecolor=z,
-    colorscale='purples',
-    contours = {
-        # "x": {"show": True, "start": -k, "end": k, "size": 0.1},
-        "y": {"show": True, "start": -th, "end": 0, "size": 0.1},
-        "z": {"show": True, "start":  0, "end": k, "size": 0.1}
-    }
-)
+# N = 1000
+# x = np.linspace(-k/sqrt(2), k/sqrt(2), N)
+# y = np.linspace(0, -th, N)
+# x, y = np.meshgrid(x, y)
+# z = x**2
+
+# fig.add_surface(
+#     x=x, y=y, z=z,
+#     showscale=False, 
+#     surfacecolor=z,
+#     colorscale='purples',
+#     contours = {
+#         # "x": {"show": True, "start": -k, "end": k, "size": 0.1},
+#         "y": {"show": True, "start": -th, "end": 0, "size": 0.1},
+#         "z": {"show": True, "start":  0, "end": k, "size": 0.1}
+#     }
+# )
 
 
 
